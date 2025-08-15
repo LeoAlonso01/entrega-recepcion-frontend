71d1a1804d220316b257e46222b6ace291867fe4
LeoAlonso01 <ing.leonardo.alonso@gmail.comm>
2025-08-14 22:28:20 -0600

    coregiido la obtencion del id por unidad

--- app/services/get_unidades.ts
+++ app/services/get_unidades.ts
@@ -11,25 +11,25 @@ export interface Unidad {
     responsable: Responsable;
 }
 
-export async function UnidadesPorUsuario(usuarioId: number): Promise<Unidad>{
-    const response = await fetch(`${API_URL}/unidad_por_usuario/${usuarioId}/unidad`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-
-    if (!response.ok) {
-        throw new Error("Error al obtener la unidad")
+export async function UnidadesPorUsuario(usuarioId: number): Promise<Unidad> {
+  const response = await fetch(`${API_URL}/unidad_por_usuario/${usuarioId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
     }
+  });
 
-    // solo se responden los datos necesarios
-    const data = await response.json()
-      return {
+  if (!response.ok) {
+    throw new Error("Error al obtener la unidad");
+  }
+
+  const data = await response.json();
+
+  return {
     id_unidad: data.id_unidad,
     nombre: data.nombre,
     responsable: {
-      id: data.responsable_id ?? data.responsable?.id,
+      id: data.responsable?.id || 0,
       nombre: data.responsable?.nombre || ""
     }
   };
